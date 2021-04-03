8a2fed3e916c2bdf25672468200fc416117faf33
Dan Pappo <dpappo@gmail.com>
2021-04-03 17:44:06 -0400

    more tests

--- boat.rb
+++ boat.rb
@@ -1,2 +1,5 @@
 class Boat
+  def allowed_aboard?(inventory)
+    inventory.include?('life jacket')
+  end
 end
--- boat_spec.rb
+++ boat_spec.rb
@@ -6,4 +6,17 @@ describe Boat do
     expect(Boat.new).to be_a Boat
   end
 
+  describe '#allowed_aboard' do
+    it 'returns true if inventory includes a life jacket' do
+      a_boat = Boat.new
+      allowed = a_boat.allowed_aboard?(['life jacket', 'sun glasses'])
+      expect(allowed).to be true
+    end
+    it 'returns false if inventory does not include a life jacket' do
+      a_boat = Boat.new
+      allowed = a_boat.allowed_aboard?(['swim trunks', 'flippy floppies'])
+      expect(allowed).to be false
+    end
+  end
+
 end
